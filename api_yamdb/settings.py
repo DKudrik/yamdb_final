20552ffdf73647f8bd087f43a4519b904611a675
ВашÐÐÐDenis Kudrik <deniskudrik@gmail.com>
2021-04-09 15:06:18 +0300

    Attempt 22

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': '${{ secrets.POSTGRES_USER }}',
+        'PASSWORD': '${{ secrets.POSTGRES_PASSWORD }}',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
